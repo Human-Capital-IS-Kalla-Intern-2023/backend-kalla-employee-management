48372f1d6570cf40dcf21452e6a241dfb68f4f1b
Ahmad Afrisal <ahmadrajendraa@gmail.com>
2023-08-24 14:08:35 +0800

    Menambahkan Model dan Migration Tabel Directorats

---
+++ app/Models/Directorat.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Directorat extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['directorat_name'];
+
+    /**
+     * Get the Postion that owns the Directorat
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function Postion(): BelongsTo
+    {
+        return $this->belongsTo(Position::class, 'directorat', 'id');
+    }
+}
