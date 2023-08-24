88ec220420fa713e36d58089e4e2827fc9c43a2f
Ahmad Afrisal <ahmadrajendraa@gmail.com>
2023-08-24 14:00:51 +0800

    Memnambahkan Model dan Migration Company

---
+++ app/Models/Company.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Company extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['company_name', 'locations_id'];
+
+    /**
+     * Get all of the comments for the Company
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function Location(): HasMany
+    {
+        return $this->hasMany(Location::class);
+    }
+}
