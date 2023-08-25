58d1eaec5129cfc003180dc30580be271b533530
Ahmad Afrisal <ahmadrajendraa@gmail.com>
2023-08-25 08:30:31 +0800

    Membuat Seeder Table Companies

---
+++ database/seeders/CompanySeeder.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Company;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CompanySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $companies = [
+            [
+                'company_name' => 'KALLA TOYOTA',
+                'locations_id' => '1',
+                'created_at' => date('Y-m-d H:i:s', time()),
+                'updated_at' => date('Y-m-d H:i:s', time()),
+            ],
+            [
+                'company_name' => 'KALLA KARS',
+                'locations_id' => '2',
+                'created_at' => date('Y-m-d H:i:s', time()),
+                'updated_at' => date('Y-m-d H:i:s', time()),
+            ],
+            [
+                'company_name' => 'OTOEXPERT',
+                'locations_id' => '3',
+                'created_at' => date('Y-m-d H:i:s', time()),
+                'updated_at' => date('Y-m-d H:i:s', time()),
+            ],
+            [
+                'company_name' => 'KALLA TRANSPORT',
+                'locations_id' => '4',
+                'created_at' => date('Y-m-d H:i:s', time()),
+                'updated_at' => date('Y-m-d H:i:s', time()),
+            ],
+            [
+                'company_name' => 'KALLA LOGISTICS',
+                'locations_id' => '4',
+                'created_at' => date('Y-m-d H:i:s', time()),
+                'updated_at' => date('Y-m-d H:i:s', time()),
+            ],
+            [
+                'company_name' => 'KALLA LINES',
+                'locations_id' => '5',
+                'created_at' => date('Y-m-d H:i:s', time()),
+                'updated_at' => date('Y-m-d H:i:s', time()),
+            ],
+            [
+                'company_name' => 'CAHAYA BONE',
+                'locations_id' => '6',
+                'created_at' => date('Y-m-d H:i:s', time()),
+                'updated_at' => date('Y-m-d H:i:s', time()),
+            ],
+            [
+                'company_name' => 'KALLA ASPAL',
+                'locations_id' => '7',
+                'created_at' => date('Y-m-d H:i:s', time()),
+                'updated_at' => date('Y-m-d H:i:s', time()),
+            ],
+            [
+                'company_name' => 'KALLA BETON',
+                'locations_id' => '8',
+                'created_at' => date('Y-m-d H:i:s', time()),
+                'updated_at' => date('Y-m-d H:i:s', time()),
+            ],
+            [
+                'company_name' => 'PT BUMI KARSA',
+                'locations_id' => '9',
+                'created_at' => date('Y-m-d H:i:s', time()),
+                'updated_at' => date('Y-m-d H:i:s', time()),
+            ],
+            [
+                'company_name' => 'WISMA KALLA',
+                'locations_id' => '10',
+                'created_at' => date('Y-m-d H:i:s', time()),
+                'updated_at' => date('Y-m-d H:i:s', time()),
+            ],
+            [
+                'company_name' => 'SAORAJA BALLROOM',
+                'locations_id' => '3',
+                'created_at' => date('Y-m-d H:i:s', time()),
+                'updated_at' => date('Y-m-d H:i:s', time()),
+            ],
+            [
+                'company_name' => 'BMS',
+                'locations_id' => '4',
+                'created_at' => date('Y-m-d H:i:s', time()),
+                'updated_at' => date('Y-m-d H:i:s', time()),
+            ]
+        ];
+
+        Company::insert($companies);
+    }
+    
+}
