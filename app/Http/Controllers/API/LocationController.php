72087d970c7752034d3733ea8dee42470a5f9c56
Ahmad Afrisal <ahmadrajendraa@gmail.com>
2023-08-25 10:55:05 +0800

    Membuat API Location

---
+++ app/Http/Controllers/API/LocationController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Helpers\ResponseFormatter;
+use App\Http\Controllers\Controller;
+use App\Models\Location;
+use Illuminate\Http\Request;
+
+class LocationController extends Controller
+{
+    public function all(Request $request) {
+        $id = $request->input('id');
+        $location_name = $request->input('location_name');
+
+        if($id) {
+            $location = Location::find($id);
+
+            if($location) {
+                return ResponseFormatter::success(
+                    $location,
+                    'Data Berhasil Diambil'
+                );
+            } else {
+                return ResponseFormatter::error(
+                    null,
+                    'Data produk tidak ada',
+                    404
+                );
+            }
+
+        }
+
+        $location = Locationz::all();
+    }
+}
