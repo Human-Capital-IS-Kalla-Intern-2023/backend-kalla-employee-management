d7f92a95295eab04bc71a9d5dce090e0bb41e30c
Eric Kwandy <erickwandy@gmail.com>
2023-08-25 20:58:52 +0800

    Menambah tabel dan model database employee

---
+++ database/migrations/2023_08_25_080931_create_employees_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->string('nip');
+            $table->string('fullname');
+            $table->string('nickname');
+            $table->date('hire_date');
+            $table->string('company_email');
+            $table->foreignId('main_position')->constrained('positions');
+            $table->foreignId('secondary_position')->constrained('positions');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('employees');
+    }
+};
