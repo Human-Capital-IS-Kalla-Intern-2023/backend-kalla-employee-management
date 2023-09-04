5fe629339490adf541cff890da02bf17caae36ff
Eric Kwandy <erickwandy@gmail.com>
2023-09-04 20:25:00 +0800

    Update Employee Controller

---
+++ app/Http/Controllers/API/EmployeeController.php
@@ -0,0 +1,137 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Helpers\ResponseFormatter;
+use App\Models\Employee;
+use Illuminate\Http\Request;
+use Exception;
+use Illuminate\Support\Facades\Validator;
+
+class EmployeeController extends Controller
+{
+    public function index(Request $request) {
+        
+        $employee = Employee::all();
+
+
+        return ResponseFormatter::success(
+            $employee,
+            'Data Perusahaan berhasil diambil'
+        );
+
+       
+    }
+
+    public function store(Request $request) {
+        try {
+            //define validation rules
+            $validator = Validator::make($request->all(), [
+                'nip' => 'required|string',
+                'fullname' => 'required|string',
+                'nickname' => 'required|string',
+                'hire_date' => 'required|date',
+                'company_email' => 'required|email',
+            ]);
+
+             //check if validation fails
+            if ($validator->fails()) {
+                return ResponseFormatter::error([
+                    'message' => 'Validation Error',
+                    'error' => $validator->errors(),
+                ], 'Validation Error', 422);
+            }
+
+
+            $data = Employee::create([
+                'nip' => $request->nip,
+            ]);
+
+
+            return ResponseFormatter::success(
+                $data,
+                'Data Berhasil Ditambahkan'
+            );
+
+        } catch (Exception $error) {
+            return ResponseFormatter::error([
+                        'message' => 'Something went wrong',
+                        'error' => $error,
+            ], 'Error', 500);
+        }
+    }
+
+    public function show(string $id)
+    {
+        try {
+
+            $employee = Employee::findOrFail($id);
+    
+            return ResponseFormatter::success(
+                $employee,
+                'Data berhasil diambil'
+            );
+        } catch (Exception $error) {
+            return ResponseFormatter::error([
+                        'message' => 'Something went wrong',
+                        'error' => $error,
+            ], 'Error', 500);
+        }
+    }
+
+    public function update(Request $request, $id) {
+        try {
+
+            //define validation rules
+            $validator = Validator::make($request->all(), [
+                'location_name'     => 'required|string',
+            ]);
+
+             //check if validation fails
+            if ($validator->fails()) {
+                return ResponseFormatter::error([
+                    'message' => 'Validation Error',
+                    'error' => $validator->errors(),
+                ], 'Validation Error', 422);
+            }
+    
+            $item = Employee::findOrFail($id);
+            
+            $item->update([
+                'nip' => $request->nip,
+            ]);
+    
+            return ResponseFormatter::success(
+                $item,
+                'Data Berhasil Diubah'
+            );
+        } catch (Exception $error) {
+            return ResponseFormatter::error([
+                        'message' => 'Something went wrong',
+                        'error' => $error,
+            ], 'Error', 500);
+        }
+        
+    }
+
+    public function destroy(string $id)
+    {
+        try {
+            
+
+            $employee = Employee::findOrFail($id);
+
+            //delete post
+            $employee->delete();
+
+            return ResponseFormatter::success(
+                'Data Berhasil Dihapus'
+            );
+        } catch (Exception $error) {
+            return ResponseFormatter::error([
+                        'message' => 'Something went wrong',
+                        'error' => $error,
+            ], 'Error', 500);
+        }
+    }
+}
