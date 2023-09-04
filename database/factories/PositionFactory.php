5b9b00940999a28d7d567a8f1a85057c9b4b8e98
Eric Kwandy <erickwandy@gmail.com>
2023-09-04 22:10:16 +0800

    Fix: division & position factory

--- database/factories/PositionFactory.php
+++ database/factories/PositionFactory.php
@@ -19,7 +19,7 @@ class PositionFactory extends Factory
         return [
             'companies_id' => rand(1, 100),
             'position_name'=> $this->faker->company(),
-            'directorate' => $this->faker->company(),
+            'directorate' => rand(1, 10),
             'division' => rand(1, 10),
             'section' => rand(1,10)
         ];
