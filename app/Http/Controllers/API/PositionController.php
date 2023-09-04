2df621f675dcf8d33e011733df010f4792d0dd09
Eric Kwandy <erickwandy@gmail.com>
2023-09-04 20:25:28 +0800

    update Position Controller

---
+++ app/Http/Controllers/API/PositionController.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Helpers\ResponseFormatter;
+use App\Models\Position;
+use Illuminate\Http\Request;
+use Exception;
+use Illuminate\Support\Facades\Validator;
+
+class PositionController extends Controller
+{
+    public function index(Request $request) {
+        
+        $position = Position::all();
+
+
+        return ResponseFormatter::success(
+            $position,
+            'Data Perusahaan berhasil diambil'
+        );
+
+       
+    }
+
+    public function store(Request $request) {
+        try {
+            //define validation rules
+            $validator = Validator::make($request->all(), [
+                'position_name'     => 'required|string',
+            ]);
+
+             //check if validation fails
+            if ($validator->fails()) {
+                return ResponseFormatter::error([
+                    'message' => 'Validation Error',
+                    'error' => $validator->errors(),
+                ], 'Validation Error', 422);
+            }
+
+
+            $data = Position::create([
+                'position_name' => $request->position_name,
+            ]);
+
+
+            return ResponseFormatter::success(
+                $data,
+                'Data Berhasil Ditambahkan'
+            );
+
+        } catch (Exception $error) {
+            return ResponseFormatter::error([
+                        'message' => 'Something went wrong',
+                        'error' => $error,
+            ], 'Error', 500);
+        }
+    }
+
+    public function show(string $id)
+    {
+        try {
+
+            $position = Position::findOrFail($id);
+    
+            return ResponseFormatter::success(
+                $position,
+                'Data berhasil diambil'
+            );
+        } catch (Exception $error) {
+            return ResponseFormatter::error([
+                        'message' => 'Something went wrong',
+                        'error' => $error,
+            ], 'Error', 500);
+        }
+    }
+
+    public function update(Request $request, $id) {
+        try {
+
+            //define validation rules
+            $validator = Validator::make($request->all(), [
+                'position_name'     => 'required|string',
+            ]);
+
+             //check if validation fails
+            if ($validator->fails()) {
+                return ResponseFormatter::error([
+                    'message' => 'Validation Error',
+                    'error' => $validator->errors(),
+                ], 'Validation Error', 422);
+            }
+    
+            $item = Position::findOrFail($id);
+            
+            $item->update([
+                'position_name' => $request->position_name,
+            ]);
+    
+            return ResponseFormatter::success(
+                $item,
+                'Data Berhasil Diubah'
+            );
+        } catch (Exception $error) {
+            return ResponseFormatter::error([
+                        'message' => 'Something went wrong',
+                        'error' => $error,
+            ], 'Error', 500);
+        }
+        
+    }
+
+    public function destroy(string $id)
+    {
+        try {
+            
+
+            $position = Position::findOrFail($id);
+
+            //delete post
+            $position->delete();
+
+            return ResponseFormatter::success(
+                'Data Berhasil Dihapus'
+            );
+        } catch (Exception $error) {
+            return ResponseFormatter::error([
+                        'message' => 'Something went wrong',
+                        'error' => $error,
+            ], 'Error', 500);
+        }
+    }
+}
