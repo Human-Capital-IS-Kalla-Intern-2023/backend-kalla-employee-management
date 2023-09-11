e9a4a3252fc2796396794262ce5680dbdef5e5a4
Ahmad Afrisal <ahmadrajendraa@gmail.com>
2023-09-11 08:53:41 +0800

    Menambahkan SoftDeletes Pada Tabel Company, Directorat, JobGrade, Location

--- app/Models/JobGrade.php
+++ app/Models/JobGrade.php
@@ -5,12 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class JobGrade extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
-    protected $fillable = ['salary'];
+    protected $fillable = ['grade_name'];
 
 
     /**
