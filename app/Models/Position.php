eeac74cc41c31ed80a66ca889e4d4a49ec45d6dc
Eric Kwandy <erickwandy@gmail.com>
2023-09-11 16:58:13 +0800

    Fix: position model

--- app/Models/Position.php
+++ app/Models/Position.php
@@ -10,7 +10,7 @@ class Position extends Model
     use HasFactory;
     protected $fillable = ['position_name'];
 
-    public function Position(){
+    public function Employee(){
         return $this->belongsTo(Employee::class, 'id', 'position');
     }
 
