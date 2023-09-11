fa72df7303932fa401ac12b6d3e5abd2032dd37f
Eric Kwandy <erickwandy@gmail.com>
2023-09-11 13:34:30 +0800

    Fix: position & employee table

--- database/migrations/2023_08_25_081030_create_positions_table.php
+++ database/migrations/2023_08_25_081030_create_positions_table.php
@@ -20,7 +20,6 @@ return new class extends Migration
             $table->unsignedBigInteger('division');
             $table->unsignedBigInteger('section');
             $table->boolean('primary');
-            $table->unsignedBigInteger('employee_id');
             $table->timestamps();
 
             $table->foreign('job_grade')->references('id')->on('job_grades')->onDelete('restrict');
