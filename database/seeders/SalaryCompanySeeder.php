5351d4a914113b7fc786bba9a8adb4b6f4814593
Eric Kwandy <erickwandy@gmail.com>
2023-09-20 10:10:57 +0800

    salary company seeder

---
+++ database/seeders/SalaryCompanySeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\SalaryCompany;
+use App\Models\SalaryComponent;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class SalaryCompanySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+
+        $components = SalaryComponent::all();
+
+
+        foreach ($components as $component) {
+            SalaryCompany::create([
+            'slug' => $component->slug,
+            'component' => $component->component_name,
+            'type' => $component->type,
+            'is_hide' => $component->is_hide,
+            'is_edit' => $component->is_edit,
+            'is_active' => $component->is_active,
+            'created_at' => $component->created_at,
+            'updated_at' => $component->updated_at,
+        ]);
+        }
+    }
+}
