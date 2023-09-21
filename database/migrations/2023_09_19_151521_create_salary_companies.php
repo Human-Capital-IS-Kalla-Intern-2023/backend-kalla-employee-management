3b82377a4d56d039018eee69ea1e6e2a2df70e8f
Eric Kwandy <erickwandy@gmail.com>
2023-09-21 13:19:11 +0800

    perbaikan tabel salary company

--- database/migrations/2023_09_19_151521_create_salary_companies.php
+++ database/migrations/2023_09_19_151521_create_salary_companies.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('salary_companies', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('company_id')->constrained();
+            $table->unsignedBigInteger('company_id')->constrained();
             $table->string('order');
             $table->string('component');
             $table->enum('type',['fixed pay','deductions']);
@@ -23,7 +23,7 @@ return new class extends Migration
             $table->timestamps();
             $table->softDeletes();
 
-            $table->foreign('id')->references('id')->on('salary_components')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('company_id')->references('id')->on('companies')->onDelete('restrict')->onUpdate('restrict');
         });
     }
 
