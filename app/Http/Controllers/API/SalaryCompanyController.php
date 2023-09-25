dd802d031ee10d3eea7f33bef85f216778713d2f
Eric Kwandy <erickwandy@gmail.com>
2023-09-25 08:15:32 +0800

    Perbaikan salary company controller

--- app/Http/Controllers/API/SalaryCompanyController.php
+++ app/Http/Controllers/API/SalaryCompanyController.php
@@ -17,10 +17,10 @@ class SalaryCompanyController extends Controller
     {
         $search = $request->get('search');
 
-        $components = SalaryCompany::query()->when($search, function($query) use($search) {
-            $query->where('component','like','%'.$search.'%');
+        $components = SalaryCompany::query()->when($search, function ($query) use ($search) {
+            $query->where('component', 'like', '%' . $search . '%');
         })->get();
-        
+
 
         return response()->json([
             'status_code' => 200,
@@ -45,15 +45,15 @@ class SalaryCompanyController extends Controller
     {
 
         $validation = $request->validate([
-            'component' => ['required','unique:salary_companies,component,NULL,id,deleted_at,NULL','string'],
-            'company_id' => ['required','string'],
-            'order' => ['required','string'],
-            'type' => ['required','in:fixed pay,deductions'],
-            'is_hide' => ['required','boolean'],
-            'is_edit' => ['required','boolean'],
-            'is_active' => ['required','boolean'],
+            'component' => ['required', 'unique:salary_companies,component,NULL,id,deleted_at,NULL', 'string'],
+            'company_id' => ['required', 'string'],
+            'order' => ['required', 'string'],
+            'type' => ['required', 'in:fixed pay,deductions'],
+            'is_hide' => ['required', 'boolean'],
+            'is_edit' => ['required', 'boolean'],
+            'is_active' => ['required', 'boolean'],
         ]);
-        
+
         try {
 
             $component = SalaryCompany::create([
@@ -65,29 +65,6 @@ class SalaryCompanyController extends Controller
                 'is_edit' => $request->is_edit,
                 'is_active' =>  $request->is_active
             ]);
-            
-            return response()->json([
-                'status_code' => 200,
-                'status' => 'success',
-                'message' => 'Komponen Gaji berhasil ditambahkan',
-                'data' => $component,
-            ], 200);
-         } catch (Exception $error) {
-             return response()->json([
-                 'status_code' => 500,
-                 'status' => 'error',
-                 'message' => 'Komponen gaji tidak ditemukan',
-             ], 500);
-        }
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(String $id)
-    {
-        try {
-            $component = SalaryCompany::where('company_id',$id)->orderBy('order')->get();
 
             return response()->json([
                 'status_code' => 200,
@@ -102,7 +79,29 @@ class SalaryCompanyController extends Controller
                 'message' => 'Komponen gaji tidak ditemukan',
             ], 500);
         }
-        
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(String $id)
+    {
+        try {
+            $component = SalaryCompany::where('company_id', $id)->orderBy('order')->get();
+
+            return response()->json([
+                'status_code' => 200,
+                'status' => 'success',
+                'message' => 'Komponen Gaji berhasil ditambahkan',
+                'data' => $component,
+            ], 200);
+        } catch (Exception $error) {
+            return response()->json([
+                'status_code' => 500,
+                'status' => 'error',
+                'message' => 'Komponen gaji tidak ditemukan',
+            ], 500);
+        }
     }
 
     /**
@@ -119,16 +118,16 @@ class SalaryCompanyController extends Controller
     public function update(Request $request, String $id)
     {
         $validation = $this->validate($request, [
-            'component' => ['required','string','unique:salary_companies,component,'.$id.',id,deleted_at,NULL'],
-            'company_id' => ['required','string'],
-            'order' =>['required','string'],
-            'type' => ['required','in:fixed pay,deductions'],
+            'component' => ['required', 'string', 'unique:salary_companies,component,' . $id . ',id,deleted_at,NULL'],
+            'company_id' => ['required', 'string'],
+            'order' => ['required', 'string'],
+            'type' => ['required', 'in:fixed pay,deductions'],
             'is_hide' => ['boolean'],
             'is_edit' => ['boolean'],
             'is_active' => ['boolean'],
         ]);
 
-        
+
         try {
             $component = SalaryCompany::findOrFail($id);
 
@@ -141,7 +140,18 @@ class SalaryCompanyController extends Controller
                 'is_edit' => $request->is_edit,
                 'is_active' =>  $request->is_active
             ]);
-            
+
+            // if ($component->getOriginal('order') > $component->order) {
+            //     # code...
+            //     $list = [
+            //         $component->order, $component->getOriginal('order')
+            //     ];
+            // } else {
+            //     $list = [
+            //         $component->getOriginal('order'), $component->order
+            //     ];
+            // }
+
             return response()->json([
                 'status_code' => 200,
                 'status' => 'success',
@@ -163,25 +173,23 @@ class SalaryCompanyController extends Controller
     public function destroy(String $id)
     {
 
-        try {            
+        try {
             $component = SalaryCompany::findOrFail($id);
 
             //delete post
             $component->delete();
 
             return response()->json([
-                'status_code' => 200, 
+                'status_code' => 200,
                 'status' => 'success',
                 'message' => 'Komponen berhasil dihapus',
                 'data' => $component,
             ], 200);
-
-
         } catch (Exception $error) {
 
             if ($error->getCode() == '23000') {
                 return response()->json([
-                    'status_code' => 500, 
+                    'status_code' => 500,
                     'status' => 'error',
                     'message' => 'Tidak dapat menghapus, Komponen masih digunakan tabel lain',
                 ], 500);
@@ -192,7 +200,6 @@ class SalaryCompanyController extends Controller
                 'status' => 'error',
                 'message' => 'ID Tidak ditemukan',
             ], 404);
-        
         }
     }
 }
