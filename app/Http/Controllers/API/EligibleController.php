a3f93ead3cb95b98c6509cffbff6a918e5e11020
Eric Kwandy <erickwandy@gmail.com>
2023-10-05 11:20:43 +0800

    tambahan tampilan data eligible

--- app/Http/Controllers/API/EligibleController.php
+++ app/Http/Controllers/API/EligibleController.php
@@ -3,34 +3,37 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\Eligible;
 use Illuminate\Http\Request;
 
 class EligibleController extends Controller
 {
-    public function store(Request $request) {
-        $input = [
-
-            'empl' => 'Demo Title',
-
-            'data' => [
-
-                '1' => 'One',
-
-                '2' => 'Two',
-
-                '3' => 'Three'
-
+    public function index(Request $request)
+    {
+        $salary_detail = [
+            'salary_detail' => [
+                'salary_id' => '',
+                'order' => 'Two',
+                'salary_component_id' => 'Three',
+                'component_name' => 'Three',
+                'type' => 'Three',
+                'is_hide' => 'Three',
+                'is_edit' => 'Three',
+                'is_active' => 'Three'
             ]
-
         ];
 
-        $item = Item::create($input);
+        $item = Eligible::create(
+            [
+                'salary_detail' => json_encode($salary_detail)
+            ]
+        );
 
         return response()->json([
             'status_code' => 200,
             'status' => 'success',
             'message' => 'Karyawan baru berhasil diambil',
-            'data' => $employee,
+            'data' => $item,
         ], 200);
     }
 }
