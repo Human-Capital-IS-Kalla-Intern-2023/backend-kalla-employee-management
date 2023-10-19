d4a433a159b2d47f2bfe1e511f7322c68bca601e
Ahmad Afrisal <ahmadrajendraa@gmail.com>
2023-10-19 13:06:22 +0800

    Membuat Model & Migration Compensations, EmployeeCompensations

---
+++ database/migrations/2023_10_19_120258_create_employee_compensations_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('employee_compensations', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('compensation_id')->constrained();
+            $table->json('employee');
+            $table->json('position');
+            $table->json('eligble');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('employee_compensations');
+    }
+};
